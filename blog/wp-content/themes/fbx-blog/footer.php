a4ee0130b104ba1109362d983c103ba109fb0e67
Lewis Healey <lewis.healey@firebox.com>
2017-04-27 11:37:10 +0100

    Single post amendment

--- blog/wp-content/themes/fbx-blog/footer.php
+++ blog/wp-content/themes/fbx-blog/footer.php
@@ -15,6 +15,7 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="<?php echo get_template_directory_uri();?>/lib/masonry.pkgd.min.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/lib/imagesloaded.pkgd.min.js"></script>
 <script src="<?php echo get_template_directory_uri();?>/lib/fitvid.js"></script>
 <script src="<?php echo get_template_directory_uri();?>/lib/main.js"></script>
 	<?php wp_footer(); ?>
--- blog/wp-content/themes/fbx-blog/single.php
+++ blog/wp-content/themes/fbx-blog/single.php
@@ -11,13 +11,30 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
           <h1 class="single-post__title"><?php the_title(); ?></h1>
-          <div class="single-post__breadcrumbs">Breadcrumbs</div>
-          <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large',['class' => 'single-post__feature-image']); } ?>
+          <div class="single-post__breadcrumbs">
+            <?php if ( function_exists('yoast_breadcrumb') ) {
+            yoast_breadcrumb('<p id="breadcrumbs">','</p>'); } ?></div>
+          <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large',['class' => 'single-post__feature-image']); $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id() ); } ?>
 
           <div class="single-post__action hidden-xs">
             <div class="row">
               <div class="col-xs-12 col-md-6 single-post__social">
-                Share post <i class="fa fa-facebook"></i><i class="fa fa-twitter"></i><i class="fa fa-instagram"></i><i class="fa fa-pinterest"></i><i class="fa fa-google-plus"></i>
+                Share post
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo the_permalink(); ?>" title="Share on Facebook">
+                  <i class="fa fa-facebook"></i>
+                </a>
+                <a href="https://twitter.com/home?status=<?php the_title(); ?>: <?php echo the_permalink(); ?>" title="Share on Twitter">
+                  <i class="fa fa-twitter"></i>
+                </a>
+                <a href="https://plus.google.com/share?url=<?php echo the_permalink(); ?>" title="Share on Facebook">
+                  <i class="fa fa-instagram"></i>
+                </a>
+                <a href="https://pinterest.com/pin/create/button/?url=<?php echo $feat_image_url; ?>&media=<?php the_title(); ?>&description=<?php echo excerpt(20); ?>" title="Share on Pinterest">
+                  <i class="fa fa-pinterest"></i>
+                </a>
+                <a href="https://plus.google.com/share?url=<?php echo the_permalink(); ?>" title="Share on Google Plus">
+                  <i class="fa fa-google-plus"></i>
+                </a>
               </div>
               <div class="col-xs-12 col-md-6 tar single-post__info">
                 <span>
@@ -53,36 +70,74 @@
 
       </div>
 
+
+
+
     </div>
 
   </div>
 
-</div>
+  <div class="container">
 
-<div class="grid-mobile grid-mobile--related visible-xs">
-  <?php $query = new WP_Query( array( 'post_type' => 'post' ) );  ?>
-  <?php if ( $query->have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="grid-item-mobile">
-      <div class="grid-item__thumbnail">
-        <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>">
-          <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } ?>
-        </a>
-      </div>
-      <div class="grid-item__content">
-        <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>"><h2 class="grid-item__content-title"><?php the_title(); ?></h2></a>
-        <p class="grid-item__content-excerpt"><?php echo excerpt(17); ?></p>
-        <div class="grid-item__content-links">
-          <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>" class="grid-item__content-links grid-item__content-links--read-more">Read more</a>
-          <?php $categories = get_the_category();
-          if ( ! empty( $categories ) ) {
-            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '" class="grid-item__content-links grid-item__content-links--tag">' . esc_html( $categories[0]->name ) . '</a>';
-          } ?>
+    <div class="row">
+
+      <div class="col-xs-12 col-md-12">
+
+  <div class="grid hidden-xs">
+    <div class="grid-sizer"></div>
+    <?php query_posts('post_type=post&order=DESC&posts_per_page=30');
+       if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="grid-item">
+        <div class="grid-item__thumbnail">
+          <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } ?>
+          </a>
+        </div>
+        <div class="grid-item__content">
+          <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>"><h2 class="grid-item__content-title"><?php the_title(); ?></h2></a>
+          <p class="grid-item__content-excerpt"><?php echo excerpt(17); ?></p>
+          <div class="grid-item__content-links">
+            <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>" class="grid-item__content-links grid-item__content-links--read-more">Read more</a>
+            <?php $categories = get_the_category();
+            if ( ! empty( $categories ) ) {
+              echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '" class="grid-item__content-links grid-item__content-links--tag">' . esc_html( $categories[0]->name ) . '</a>';
+            } ?>
+          </div>
         </div>
       </div>
-    </div>
-  <?php endwhile; endif; ?>
 
+    <?php endwhile; endif; wp_reset_query();?>
+  </div>
+
+  <div class="grid-mobile grid-mobile--related visible-xs">
+    <?php query_posts('post_type=post&order=DESC&posts_per_page=30');
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="grid-item-mobile">
+        <div class="grid-item__thumbnail">
+          <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } ?>
+          </a>
+        </div>
+        <div class="grid-item__content">
+          <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>"><h2 class="grid-item__content-title"><?php the_title(); ?></h2></a>
+          <p class="grid-item__content-excerpt"><?php echo excerpt(17); ?></p>
+          <div class="grid-item__content-links">
+            <a href="<?php echo the_permalink(); ?>" title="<?php the_title(); ?>" class="grid-item__content-links grid-item__content-links--read-more">Read more</a>
+            <?php $categories = get_the_category();
+            if ( ! empty( $categories ) ) {
+              echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '" class="grid-item__content-links grid-item__content-links--tag">' . esc_html( $categories[0]->name ) . '</a>';
+            } ?>
+          </div>
+        </div>
+      </div>
+    <?php endwhile; endif; wp_reset_query(); ?>
+
+  </div>
+</div>
 </div>
 
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
